234190a037aafc43e6c2123112093dbd73317576
David Hayden <dallenhayden@gmail.com>
2014-04-18 20:58:21 -0500

    Initial Commit. Following the documentation here: http://developer.android.com/training/index.html

---
+++ src/com/helloworld/DisplayMessageActivity.java
@@ -0,0 +1,48 @@
+package com.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Build;
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.support.v4.app.NavUtils;
+
+public class DisplayMessageActivity extends Activity
+{
+    @SuppressLint( "NewApi" )
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate( Bundle savedInstanceState )
+    {
+        super.onCreate( savedInstanceState );
+
+	/*
+	if( Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB )
+	{
+	    getActionBar().setDisplayHomeAsUpEnabled( true );
+	}*/
+
+	Intent intent = getIntent();
+	String message = intent.getStringExtra( HelloWorld.EXTRA_MESSAGE );
+
+	TextView textView = new TextView( this );
+	textView.setTextSize( 40 );
+	textView.setText( message );
+
+	setContentView( textView );
+    }
+
+    @Override
+    public boolean onOptionsItemSelected( MenuItem item )
+    {
+	switch( item.getItemId() )
+	{
+	    case android.R.id.home:
+		NavUtils.navigateUpFromSameTask( this );
+		return true;
+	}
+	return super.onOptionsItemSelected( item );
+    }
+}
