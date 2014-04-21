2ee37fba965b0122d00eb121170f8c74fdc8903c
David Hayden <dallenhayden@gmail.com>
2014-04-21 04:31:17 -0500

    More autogen.sh improvements

--- autogen.sh
+++ autogen.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Autogeneration script, run this script to set up the
-# project. Note that you must have ANDROID_SDK set and 
-# ANDROID_NDK set if you are using native code.
+# project. Note that you must have ANDROID_SDK set. You 
+# also need ANDROID_NDK set if you are using native code.
 
 # This script must run from the PROJECT_DIR or it will fail.
 
@@ -19,26 +19,57 @@ LIB_DIR=$PROJECT_DIR/libs
 CP_CMD="$(which cp)"
 MKDIR_CMD="$(which mkdir)"
 
-# Attempt to set the SDK and NDK if not available
+# If ANDROID_SDK is not already set, set it to this default.
+# NOTE:	Alter this if you have a different default location.
 if [ -z "$ANDROID_SDK" ]; then
+	# Change ANDROID_SDK below to the path of your SDK
 	export ANDROID_SDK=$HOME/Android/SDK	#This is a symbolic link to the SDK on my system
 fi
 
-if [ -z "$ANDROID_NDK" ]; then
-	export ANDROID_NDK=$HOME/Android/SDK	#This is a symbolic link to the NDK on my system
+# Set the android command
+ANDROID_CMD=$ANDROID_SDK/tools/android
+
+# Verify that the android tool exists and error if unavailable
+if [ ! -x "$ANDROID_CMD" ]; then
+	echo "ERROR: android sdk is unavailable."
+	echo "Be sure you've downloaded the sdk and that the ANDROID_SDK environment"
+	echo "variable is set to the unzipped sdk directory."
+	exit 1
+fi
+
+# Locations of support libraries we need from the sdk
+LIB_APPCOMPAT_SRC_DIR=$ANDROID_SDK/extras/android/support/v7/appcompat
+LIB_APPCOMPAT_INST_DIR=$LIB_DIR/appcompat
+
+# For jni projects we also need the ndk
+if [ -d "$PROJECT_DIR/jni" ]; then
+	# If ANDROID_NDK is not already set, set it to this default.
+	# NOTE:	Alter this if you have a different default location.
+	if [ -z "$ANDROID_NDK" ]; then
+		# Change ANDROID_NDK below to the path of your NDK
+		export ANDROID_NDK=$HOME/Android/NDK	#This is a symbolic link to the NDK on my system
+	fi
+
+	# Set the ndk-build command
+	NDK_BUILD_CMD=$ANDROID_NDK/ndk-build
+
+	# Verify that the ndk-build cool exists and error if unavailable
+	if [ ! -x "$NDK_BUILD_CMD" ]; then
+		echo "ERROR: ndk-build is unavailable."
+		echo "This package contains jni code which must be compiled using ndk-build."
+		echo "Be sure you've downloaded the ndk and that the ANDROID_NDK environment"
+		echo "variable is set to the unzipped ndk"
+		exit 1
+	fi
+
+	# Let's go ahead and build our native sources now
+	$NDK_BUILD_CMD
 fi
 
 # If there is no libs directory, make it now
 if [ ! -d  "$LIB_DIR" ]; then
 	$MKDIR_CMD $PROJECT_DIR/libs
 fi
-
-# Find the android command
-ANDROID_CMD=$ANDROID_SDK/tools/android
-
-# Locations of support libraries we need from the sdk
-LIB_APPCOMPAT_SRC_DIR=$ANDROID_SDK/extras/android/support/v7/appcompat
-LIB_APPCOMPAT_INST_DIR=$LIB_DIR/appcompat
 
 # Obtain the appcompat library that this project needs;
 # you must have these installed within your SDK.
